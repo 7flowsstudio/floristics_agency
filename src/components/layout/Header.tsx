d2d92869205526c4405e4f77f59bb6119d12fad0
Viktoriia <struk.victoria@gmail.com>
2026-02-24 01:23:17 +0200

    feat: add elements

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+'use client';
+import { usePathname } from 'next/navigation';
+import Container from './Container';
+import Logo from './Logo';
+import NavLink from '../navigation/NavLink';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const pathname = usePathname();
+  const isHome = pathname === '/';
 
-export default Header
+  return (
+    <header className="w-full py-8.75">
+      <Container>
+        <div className="flex items-center justify-between">
+          <Logo variant={isHome ? 'primary' : 'black'} size="sm" />
+
+          <nav className="flex gap-2">
+            <NavLink href="/about">Про мене</NavLink>
+            <NavLink href="/courses">Курси</NavLink>
+            <NavLink href="/history">Історія</NavLink>
+            <NavLink href="/reviews">Відгуки</NavLink>
+            <NavLink href="/photos">Фото</NavLink>
+            <NavLink href="/contacts">Контакти</NavLink>
+          </nav>
+          <a href="tel:+380932451284" className="text-sm font-medium">
+            093 245 1284
+          </a>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
