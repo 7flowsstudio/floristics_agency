8df5d79d84048d9615c0e7829b447f62003386fe
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2026-03-10 18:26:20 +0200

    add-big-course-page-text

--- src/components/Pages/BigCoursePage/BigCoursePage.tsx
+++ src/components/Pages/BigCoursePage/BigCoursePage.tsx
@@ -19,7 +19,6 @@ const BigCoursePage = () => {
     <Container className="pt-[100px] md:pt-[200px]">
       <h2>Food-Флористика: Від ідеї до бізнесу</h2>
       <p>Від творчості до власної справи</p>
-      <p>Коли творчість стає справою життя</p>
       <div className="">
         <Slider<BigCourseSliderItem>
           items={items}
@@ -30,11 +29,11 @@ const BigCoursePage = () => {
               key={item.id}
               className={clsx(
                 "flex-shrink-0 rounded-lg overflow-hidden relative",
-                "min-w-[320px]  [@media(max-width:374px)]:min-w-[280px] h-[440px]",
+                "min-w-[320px]  [@media(max-width:374px)]:min-w-[280px]",
               )}
             >
               {item.imageUrl && (
-                <div className="w-full h-[440px] relative rounded mb-3">
+                <div className="w-full h-[318px] relative rounded mb-3">
                   <Image
                     src={item.imageUrl}
                     alt={"food-floristic"}
@@ -46,6 +45,25 @@ const BigCoursePage = () => {
             </div>
           )}
         />
+      </div>
+      <p>Коли творчість стає справою життя</p>
+      <div>
+        <h3>Для кого цей курс</h3>
+        <ul className="list-none pl-5">
+          <li>Ти маєш творчі навички і хочеш заробляти на Food-Флористиці</li>
+          <li>Плануєш відкрити власну справу, але не знаєш, з чого почати</li>
+          <li>Боїшся цифр, але розумієш, що без них не обійтись</li>
+          <li>Хочеш ясності замість здогадок і страху помилитися</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Що ти вивчиш</h3>
+        <ul className="list-none pl-5">
+          <li>Як перетворити творчу ідею на реальний бізнес</li>
+          <li>З чого почати власну справу</li>
+          <li>Як оцінити ідею, ресурси та ризики</li>
+          <li>Впевнене прийняття бізнес-рішень</li>
+        </ul>
       </div>
     </Container>
   );
