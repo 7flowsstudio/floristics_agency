f16423379c17a79eaf28aed96868d768f00881ff
Viktoriia <struk.victoria@gmail.com>
2026-03-15 00:41:03 +0200

    fix: add relative position

--- src/app/courses/big-course/page.tsx
+++ src/app/courses/big-course/page.tsx
@@ -2,9 +2,9 @@ import BigCoursePage from "@/components/Pages/BigCoursePage/BigCoursePage";
 
 const Page = () => {
   return (
-    <>
+    <div className="relative">
       <BigCoursePage />
-    </>
+    </div>
   );
 };
 
