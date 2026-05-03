026f02f78a9072c4e8091fb9f0c055d576ff07f1
7flowsstudio <seven.flows.studio@gmail.com>
2026-05-03 21:51:49 +0200

    add environment

--- src/lib/actions/send-form-actions.ts
+++ src/lib/actions/send-form-actions.ts
@@ -5,36 +5,38 @@ import { courseFormSchema } from "@/lib/validation/course-form";
 import { CourseFormState } from "@/lib/types/forms";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465,
-  auth: {
-    user: process.env.SMTP_USER!,
-    pass: process.env.SMTP_PASSWORD!,
-  },
+	host: process.env.SMTP_HOST,
+	port: Number(process.env.SMTP_PORT),
+	secure: Number(process.env.SMTP_PORT) === 465,
+	auth: {
+		user: process.env.SMTP_USER!,
+		pass: process.env.SMTP_PASSWORD!,
+	},
 });
 
-export async function sendCourseForm(prevState: CourseFormState, formData: FormData) {
-  try {
-    const data = Object.fromEntries(formData);
+export async function sendCourseForm(
+	prevState: CourseFormState,
+	formData: FormData,
+) {
+	try {
+		const data = Object.fromEntries(formData);
 
-    const validation = courseFormSchema.safeParse(data);
+		const validation = courseFormSchema.safeParse(data);
 
-    if (!validation.success) {
-      return {
-        success: false,
-        errors: validation.error.flatten().fieldErrors,
-      };
-    
-    }
+		if (!validation.success) {
+			return {
+				success: false,
+				errors: validation.error.flatten().fieldErrors,
+			};
+		}
 
-    const { name, surname, phone, course, message } = validation.data;
+		const { name, surname, phone, course, message } = validation.data;
 
-    await transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to: process.env.SMTP_RECEIVER,
-      subject: "Нова заявка на курс",
-      html: `
+		await transporter.sendMail({
+			from: process.env.SMTP_FROM,
+			to: process.env.SMTP_RECEIVER,
+			subject: "Нова заявка на курс",
+			html: `
           <h2>Нова заявка</h2>
           <p><strong>Ім'я:</strong> ${name}</p>
           <p><strong>Прізвище:</strong> ${surname || "Не вказано"}</p>
@@ -42,17 +44,17 @@ export async function sendCourseForm(prevState: CourseFormState, formData: FormD
           <p><strong>Курс:</strong> ${course}</p>
           <p><strong>Повідомлення:</strong> ${message || "Не вказано"}</p>
         `,
-    });
+		});
 
-    return {
-      success: true,
-      errors: {},
-    }
-  } catch (error) {
-    return {
-      success: false,
-      error: "Сталася помилка при відправці форми. Спробуйте пізніше.",
-      errors: {},
-    };
-  }
+		return {
+			success: true,
+			errors: {},
+		};
+	} catch (error) {
+		return {
+			success: false,
+			error: "Сталася помилка при відправці форми. Спробуйте пізніше.",
+			errors: {},
+		};
+	}
 }
